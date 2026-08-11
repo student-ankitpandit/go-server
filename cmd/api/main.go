fcb412886e11d2c9ae2e89319cccc53887461431
Ankit Pandit <ankitkumarpandit1221@gmail.com>
2026-08-11 23:40:56 +0530

    feat: build prod ready go server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("go server is up and running...")
+	mux := http.NewServeMux()
+	
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"all ok"}`))
+	})
+
+	srv := http.Server{
+		Addr: ":3000",
+		Handler: mux,
+		ReadTimeout: time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout: time.Second * 60,
+	}
+	
+	log.Println("server is up and running on port 3000")
+	
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("sever failed to start: %v", err)	
+	}
 }
