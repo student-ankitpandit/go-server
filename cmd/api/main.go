8888d15adc16e544994b151c8496197f2a4976e8
Ankit Pandit <ankitkumarpandit1221@gmail.com>
2026-08-14 01:10:39 +0530

    feat: apply prod grade pratices to get and check env var

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/student-ankitpandit/go-server/internal/config"
 )
 
 func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println("starting go server...")
 	mux := http.NewServeMux()
-	
-	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request){
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status":"all ok"}`))
 	})
 
 	srv := http.Server{
-		Addr: ":3000",
-		Handler: mux,
-		ReadTimeout: time.Second * 10,
+		Addr:         ":" + cfg.Port,
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
-		IdleTimeout: time.Second * 60,
+		IdleTimeout:  time.Second * 60,
 	}
-	
-	log.Println("server is up and running on port 3000")
-	
+
+	log.Printf("server is up and running on port %s", srv.Addr)
+
 	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("sever failed to start: %v", err)	
+		log.Fatalf("sever failed to start: %v", err)
 	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+	Env  string
+}
+
+func MustLoad() Config { //must pattern
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("PORT is required!")
+	}
+
+	env := os.Getenv("ENV")
+	if env == "" {
+		panic("ENV is required!")
+	}
+
+	return Config{
+		Port: port,
+		Env:  env,
+	}
+}
